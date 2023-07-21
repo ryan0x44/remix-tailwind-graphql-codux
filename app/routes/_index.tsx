bfb15afa31c2ec8b5f33a88a340d193e44fc18d8
Ryan D <git@ryan0x44.com>
2023-07-21 15:53:07 +1000

    Added example of using graphql-request

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+//import { graphql } from '../graphql/gql';
+import { gql, request } from 'graphql-request'
+import graphqlClient from '../graphql-client';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -7,10 +11,29 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+const locationsQuery = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+    }
+  }
+`;
+
+export async function loader() {
+  return await graphqlClient.request(locationsQuery)
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <div>
+        { JSON.stringify(data.locations) }
+      </div>
       <ul>
         <li>
           <a
